6f48b209b184044786b7c355bb063d13051bec88
Jose Caceres <josecaceres.oreul@gmail.com>
2017-09-09 13:56:38 -0400

    Update autoload.php

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -5,15 +5,4 @@
  * Inicialización de los procedimientos de autocarga
  * compuestos por composer
  */
-
 require __DIR__.'/../vendor/autoload.php';
-
-
-
-
-
-/**
- * Autocarga de mágics y demás herramientas
- */
-
-require_once ('/../Vendor/Joalcapa/Gauler/Src/Autoload.php');
