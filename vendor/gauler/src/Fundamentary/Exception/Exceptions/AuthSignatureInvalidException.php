c3beacf544b8e38f70fbbd36bd1655e4fae04409
Jose Caceres <josecaceres.oreul@gmail.com>
2017-09-09 12:03:42 -0430

    Reestructuración

--- vendor/gauler/src/Fundamentary/Exception/Exceptions/AuthSignatureInvalidException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Fundamentary\Exception\Exceptions;
-
-use Fundamentary\Exception\Exceptions\DriverException as DriverException;
-
-class AuthSignatureInvalidException extends DriverException {
-       
-}
--- vendor/gauler/src/Fundamentary/Exception/Exceptions/KillerException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Fundamentary\Exception\Exceptions;
-
-use Fundamentary\Exception\Exceptions\DriverException as DriverException;
-
-class KillerException extends DriverException {
-    
-}
