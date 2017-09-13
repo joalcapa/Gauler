eb98040c9a5a23b5ed269707b51c940e5d07625a
Jose Caceres <josecaceres.oreul@gmail.com>
2017-09-13 08:18:48 -0430

    add HTTPS

--- api/hypermedia.php
+++ api/hypermedia.php
@@ -12,10 +12,19 @@
 | el atributo $id del usuario, puesto que sustituye el espacio de nombre
 | {{id_user}} por el $id del estado temporal, puesto que Gauler no almacena
 | sesion alguna.
+|
+| Además a esto, Gauler enviará los vínculos de hypermedia, utilizando
+| siempre el protocolo HTTPS SEGURO, por lo que el servidor de producción
+| deberá tener los certificados SSL para el protocolo HTTPS SEGURO,
+| ya que Gauler siendo una api RestFull, trabajará en el puerto 443,
+| recuerde colocar solo el dominio con o sin www, el protocolo será ajustado por Gauler,
+| por DEFAULT, Gauler edita el archivo .htacces, para redireccionar a HTTPS,
+| por lo que debes verificar que el modulo Rewrite este habilitado,
+| por default muchos servidores de producción lo mantienen habilitado.
 */
 return [
     'SuperUser' => [
-        'urlPassword' => env('HOST_API','http://localhost/app/public').'/api/users/{{id_user}}/resetpassword',
-        'urlUsers' => env('HOST_API','http://localhost/app/public').'/api/users/{{id_user}}',
+        'urlPassword' => env('HOST_API','localhost/app/public').'/api/users/{{id_user}}/resetpassword',
+        'urlUsers' => env('HOST_API','localhost/app/public').'/api/users/{{id_user}}',
     ]
 ];
