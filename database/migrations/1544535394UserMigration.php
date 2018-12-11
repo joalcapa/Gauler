0c44d170aabc7f146924c15d7baea664552245dd
Jose Caceres <josecaceres.oreul@gmail.com>
2018-12-11 14:53:46 +0100

    Update 1544535394UserMigration.php

--- database/migrations/1544535394UserMigration.php
+++ database/migrations/1544535394UserMigration.php
@@ -8,9 +8,9 @@ use Joalcapa\Origins\App\DataBase\Types\DBTypes as DBTypes;
 class UserMigration extends Migration {
 
 	public $attributes = [
-		'name' => DBTypes::string,
-		'email' => DBTypes::string,
-		'password' => DBTypes::string
+	     'name' => DBTypes::string,
+	     'email' => DBTypes::string,
+	     'password' => DBTypes::string
 	];
 
 	public $dependencias = [
